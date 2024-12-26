d6b16b6b401b72097e011352d1ccde81b2fbe56e
David <davidagamez04@gmail.com>
2024-12-26 08:26:31 -0500

    refactor: update AppModule for microservice architecture with NATS transport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
+import { AuthModule } from './auth/auth.module';
+import { envs } from './config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PermissionModule } from './permission/permission.module';
+import { RoleModule } from './role/role.module';
+import { SeedModule } from './seed/seed.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'authentication-database',
+      port: 5432,
+      database: 'authdb',
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+    SeedModule,
+    RoleModule,
+    PermissionModule,
+  ],
 })
 export class AppModule {}
